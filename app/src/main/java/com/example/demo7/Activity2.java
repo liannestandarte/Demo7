49ff8c3bfb054e534eb12a0f4d426a6a4c7f7948
cisco 2018 <cisco2018@IICSGENERAL.com>
2019-07-09 13:07:09 +0800

    musssssyboi

--- app/src/main/java/com/example/demo7/Activity2.java
+++ app/src/main/java/com/example/demo7/Activity2.java
@@ -6,8 +6,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -31,4 +37,38 @@ public class Activity2 extends AppCompatActivity {
             txtName.setText(sp.getString("user",""));
             txtPassword.setText(sp.getString("pwd",""));
         }
+
+
+    public void displayInternal (View v) {
+        FileInputStream fis = null;
+        StringBuffer buffer = new StringBuffer();
+        int letter = 0;
+        try {
+            fis = openFileInput("user.txt");
+            while ((letter = fis.read()) != -1) {
+                buffer.append((char)letter);
+            }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        //  Toast.makeText(this, buffer, Toast.LENGTH_LONG).show();
+        //String name, password, str;
+        //byte[] str = buffer.toString().getBytes();
+        //while (str != Integer.parseInt(",")) {}
+        //String name, password, str;
+        //str = buffer.toString();
+        //name = str.substring(0, str.indexOf(","));
+        //password = str.substring(str.indexOf(",")+1);
+        txtName.setText(buffer.substring(0,buffer.indexOf(",")));
+        txtPassword.setText(buffer.substring(buffer.indexOf(",")+1));
+    }
 }
+
